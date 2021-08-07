5bba51c7b48a45525fc9661ec4bbe5aa3f3720d1
Taylan Postalci <taylanpostalci420@gmail.com>
2021-08-07 12:24:35 -0400

    changestojs

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,55 +9,82 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+  var upperCase = false
+  var lowerCase = false
+  var specialChar = false
+  var questionNotDone = true;
+
   var pwlength =
     prompt("Please specify the length in which you want your password to be \n You may choose any length between 8 and 128");
 
 
-  while (isNaN(parseInt(pwlength)) || (parseInt(pwlength) < 8 || parseInt(pwlength) > 128)) {
+  while (pwlength < 8 || pwlength > 128) {
     pwlength = prompt("please enter an interger between 8 and 128");
 
 
   }
 
-  var upperCase = false
-  var lowerCase = false
-  var specialChar = false
-
-
-  var input = prompt("Do you want your password to include uppercase letters \n Type Y for yes and N for no")
-  if (input.toUpperCase() == "Y") {
-    upperCase = true
-  }
-  var input = prompt("Do you want your password to include lowercase letters \n Type Y for yes and N for no")
-  if (input.toUpperCase() == "Y") {
-    lowerCase = true
-  }
-  var input= prompt("Do you want your password to include special characters? \n Type Y for yes and N for no")
-  if (input.toUpperCase() == "Y") {
-    specialChar = true
-  }
-  while (upperCase == false && lowerCase == false && specialChar == false) {
-    alert("Please select atleast one option to strengthen your password!")
-    var input = prompt("Do you want your password to include uppercase letters \n Type Y for yes and N for no")
-    if (input.toUpperCase() == "Y") {
-      upperCase = true
+  while (questionNotDone) {
+    var input = prompt("Do you want your password to include upper case letters \n Type Y for yes and N for no")
+    while (input.toUpperCase().compareTo("Y") !== 0 && input.toUpperCase().compareTo("N") !== 0) {
+      alert("Please input a Y or N!");
+      input = prompt("Do you want your password to include upper case characters? \n Type Y for yes and N for no")
     }
-    var input = prompt("Do you want your password to include lowercase letters \n Type Y for yes and N for no")
-    if (input.toUpperCase() == "Y") {
-      lowerCase = true
+    if (input.toUpperCase().compareTo("Y") == 0) {
+      upperCase = true;
     }
-    var input = prompt("Do you want your password to include special characters? \n Type Y for yes and N for no")
-    if (input.toUpperCase() == "Y") {
-      specialChar = true
+    else
+      upperCase = false;
+
+
+
+    var input2 = prompt("Do you want your password to include lower case letters \n Type Y for yes and N for no")
+    while (input2.toUpperCase().compareTo("Y") != 0 && input2.toUpperCase().compareTo("N") != 0) {
+      alert("Please input a Y or N!");
+      input2 = prompt("Do you want your password to include lower case characters? \n Type Y for yes and N for no")
     }
+    if (input2.toUpperCase().compareTo("Y") == 0) {
+      lowerCase = true;
+    }
+    else
+      lowerCase = false;
+
+
+
+    var input3 = prompt("Do you want your password to include special characters? \n Type Y for yes and N for no")
+
+
+    while (input3.toUpperCase().compareTo("Y") != 0 && input3.toUpperCase().compareTo("N") != 0) {
+      alert("Please input a Y or N!");
+      input3 = prompt("Do you want your password to include special characters? \n Type Y for yes and N for no")
+    }
+    if (input3.toUpperCase().compareTo("Y") == 0) {
+      specialChar = true;
+    }
+    else
+      specialChar = false;
+
+
+    if (lowerCase || upperCase || specialChar){
+
+    questionNotDone = false;
+    }
+
 
 
   }
-
-  console.log(parseInt(pwlength));
-
-
+  alert("YAY");
 }
+
+
+
+
+console.log(parseInt(pwlength));
+
+
+
+
 function generatePassword() {
   console.log("somebullshit")
 
